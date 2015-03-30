c75f1dfb02a6f7f644132e9f03782783180951b0
Rene Milk <rene.milk@uni-muenster.de>
2015-03-30 15:10:16 +0200

    rename

--- python/profiler_ana/msfem.py
+++ python/profiler_ana/msfem.py
@@ -6,22 +6,20 @@ import os
 
 import pandas_common as pc
 
-# pd.options.display.mpl_style = 'default'
+
 first = sys.argv[1]
 mg = os.path.basename(first)
 merged = 'merged_{}.csv'.format(''.join(mg[:-4]))
 
-timings = ['usr', 'mix', 'sys', 'wall']
-measures = ['max', 'avg']
-specials = ['run', 'threads', 'ranks']
 baseline_name = 'msfem.all'
-sort_cols = ['ranks', 'threads']
-round_digits = 2
 
 headerlist, current = pc.read_files(sys.argv[1:])
-current = pc.sorted(current, False)
-current = pc.speedup(headerlist, current)
+current = pc.sorted_f(current, False)
+current = pc.speedup(headerlist, current, baseline_name)
 # pprint(t_sections)
+
+pc.plot_msfem(current, merged)
+
 current.transpose().to_csv(merged)
 # current.transpose().to_excel(merged+'.xls')
 # current.to_csv(merged)
