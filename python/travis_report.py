d130084af14fb324987c7b5a62e879d59d16d5ef
Tobias Leibner <tobias.leibner@uni-muenster.de>
2015-12-20 18:49:18 +0100

    [python.travis_report] temporarily add diagnostic print statements

--- python/travis_report.py
+++ python/travis_report.py
@@ -11,16 +11,22 @@ def clang_format_status(dirname):
     auth = ('renemilk', token)
     pr = os.environ['TRAVIS_PULL_REQUEST']
     slug = os.environ['TRAVIS_REPO_SLUG']
+    print("pr: ", pr)
+    print("slug: ", slug)
     if pr == 'false':
         statuses_url = 'https://api.github.com/repos/{}/statuses/{}'.format(slug, os.environ['TRAVIS_COMMIT'])
     else:
         r = requests.get('https://api.github.com/repos/{}/{}'.format(slug, pr), auth=auth)
+        print("Response:", r)
+        print(r.json())
         statuses_url = r.json()['statuses_url']
 
     r = requests.post(statuses_url,
                   auth=auth, data={"state" : "pending",
                   "description" : "Checking if clang-format has been applied to all source files",
                   "context" : "ci/clang-format"})
+    print("POST Response 1:", r)
+    print(r.json())
     try:
         fails = fc.check_dir(dirname, staged_only=False)
     except Exception as _:
@@ -32,3 +38,6 @@ def clang_format_status(dirname):
                       auth=auth, data={"state" : state,
                       "description" : "Checked if clang-format has been applied to all source files",
                       "context" : "ci/clang-format"})
+
+    print("POST Response 2:", r)
+    print(r.json())
