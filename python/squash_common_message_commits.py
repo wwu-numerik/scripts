0fbbf0722c0f60e5178812e0763e501645446bbd
Rene Milk <rene.milk@wwu.de>
2017-08-14 17:06:12 +0200

    [py] adds a script for commit squashing in pages repos

---
+++ python/squash_common_message_commits.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+import fileinput
+import os
+import subprocess
+import sys
+
+# Set to None for no filtering, otherwise commits to squash must start with this
+MSG_PREFIX = 'Updated documentation'
+
+def _reset_master():
+    new_commit = subprocess.check_output(['git', 'rev-parse', 'HEAD'], universal_newlines=True).strip()
+    subprocess.check_call(['git', 'checkout', 'master'])
+    subprocess.check_call(['git', 'reset', '--hard', new_commit])
+
+
+def first_run(base):
+    os.chdir(base)
+    env = os.environ.copy()
+    # rerun ourselves to select which actions to do on which commits
+    env['GIT_SEQUENCE_EDITOR'] = os.path.abspath(__file__)
+    #for commits getting squashed use online the first line of commit message
+    env['EDITOR'] = 'sed -i -e "1s/^/squash\!\ /" -e 1q'
+    # edit mode
+    subprocess.check_call(['git', 'rebase', '-i', '--root', 'HEAD'], env=env)
+    _reset_master()
+
+    # squash everything marked, do not open an editor
+    del env['GIT_SEQUENCE_EDITOR']
+    env['EDITOR'] = 'true'
+    subprocess.check_output(['git', 'rebase', '--autosquash', '-i', '--root', 'HEAD'], env=env)
+    _reset_master()
+
+
+def edit_mode(git_input):
+    combos = set()
+    for line in fileinput.input(files=git_input, inplace=True, backup='.orig'):
+        tokens = line.split(' ')
+        try:
+            verb, commit, message = tokens[0], tokens[1], ' '.join(tokens[2:])
+        except IndexError:
+            # not a useful line
+            continue
+        if message in combos and ((MSG_PREFIX and message.startswith(MSG_PREFIX)) or MSG_PREFIX is None):
+                line = 'r {} squash! {}'.format(commit, message)
+        else:
+            combos.add(message)
+        print(line)
+
+
+if __name__ == '__main__':
+    try:
+        base = sys.argv[1]
+    except IndexError:
+        base = os.getcwd()
+    if os.path.isdir(base):
+        first_run(base)
+    else:
+        edit_mode(base)
